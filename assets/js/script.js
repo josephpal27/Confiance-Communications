2a4087a361fcfc79bfaa573d92141d014e21c41b
Joseph Pal <josephpal27@gmail.com>
2025-09-22 15:34:51 +0530

    Awards section added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,29 @@
+
+// Functionality For Our Clients Slider
 var swiper = new Swiper(".ourClientsSlider", {
-  slidesPerView: 4,        // show exactly 7 slides
-  centeredSlides: true,    // keep active in center
-  loop: true,              // loop them
+  slidesPerView: 4, // show exactly 7 slides
+  centeredSlides: true, // keep active in center
+  loop: true,
   grabCursor: true,
   spaceBetween: 0,
 
   effect: "coverflow",
   coverflowEffect: {
-    rotate: 0,        // no rotation
-    stretch: 0,       // no stretch
-    depth: 300,       // small depth (not tunnel)
-    modifier: 1,      // lower = less tunnel
-    slideShadows: false
+    rotate: 0,
+    stretch: 0,
+    depth: 300, // small depth (not tunnel)
+    modifier: 1, // lower = less tunnel
+    slideShadows: false,
   },
 
   pagination: {
-    el: ".swiper-pagination",
+    el: ".our-clients-slider .swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".our-clients-slider .swiper-button-next",
+    prevEl: ".our-clients-slider .swiper-button-prev",
   },
 });
+
+// ----------------------------------------------------------------------------------------------------------------
